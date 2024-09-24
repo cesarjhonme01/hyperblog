7da92fa2f0633bb9f383c66ea2dee95997b8d76e
Cesar Meza <cesar.meza.nomberto@gmail.com>
2024-09-24 02:31:54 -0500

    agregue diferentes tipos de todos y componentes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos react 
-        </a>
-      </header>
+      <TodoCounter/> 
+      <TodoSearch/> 
+      <TodoList/>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      <TodoList/>
+      <CreateTodoButton/>
     </div>
   );
 }
 
-function TodoItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la llorona</p>
-      <snap>X</snap>
-      <p>onichan</p>
-    </li>
-  );
-}
 
 
 
