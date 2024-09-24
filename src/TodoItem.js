b515647c4200b99698ff44e7598dcd2699358849
Cesar Meza <cesar.meza.nomberto@gmail.com>
2024-09-24 02:34:35 -0500

    agregue diferentes tipos de todos

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+function TodoItem() {
+    return(
+      <li>
+        <span>V</span>
+        <p>Llorar con la llorona</p>
+        <snap>X</snap>
+          <p>onichan</p>
+      </li>
+    );
+  }
+  
+  export {TodoItem};
